bc2a61a7be15b98acb1e75e3d2224b4ad27c4f21
DevBaldev <triggerflax@gmail.com>
2025-04-08 18:48:53 +0530

    add backup.bash file

---
+++ scripts/backup.bash
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Backup directory
+BACKUP_DIR="$HOME/backup"
+
+# Create backup directory if it doesn't exist
+mkdir -p "$BACKUP_DIR"
+
+# List of files and directories to back up
+FILES_TO_BACKUP=(
+  "$HOME/.bashrc"
+  "$HOME/.vimrc"
+  "$HOME/.gitconfig"
+  "$HOME/.config"
+)
+
+# Backup files
+for FILE in "${FILES_TO_BACKUP[@]}"; do
+  if [ -e "$FILE" ]; then
+    cp -r "$FILE" "$BACKUP_DIR"
+    echo "Backed up $FILE to $BACKUP_DIR"
+  else
+    echo "File $FILE does not exist and was not backed up"
+  fi
+done
+
+echo "Backup completed!"
+```
