f6ab7fb7c5bc2b5c368c6539ebf24ea24fd8d06f
DevBaldev <triggerflax@gmail.com>
2025-04-08 20:50:00 +0530

    add fonts to install

--- scripts/install_packages.bash
+++ scripts/install_packages.bash
@@ -2,6 +2,13 @@
 
 # List of packages to install
 PACKAGE_TO_INSTALL=(
+  # Fonts
+  "noto-fonts"
+  "noto-fonts-cjk"
+  "noto-fonts-emoji"
+  "noto-fonts-extra"
+  "ttf-cascadia-code-nerd"
+  # Fish
   "fish"
   "starship"
   "lsd"
